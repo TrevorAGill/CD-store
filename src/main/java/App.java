5bfe026dac177f1f3860cdfe1ba5bb043b53a62a
Stephanie Mayer and Trevor Gill <trevor@>
2017-08-08 11:36:38 -0700

    add everything

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,102 +1,38 @@
 import models.CD;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.ArrayList;
 
-/**
- * Created by Guest on 7/31/17.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
 
-        CD Rock = new CD ("Ted Nuget", "Just The Best",2005,5);
-        CD Rap = new CD ("Tupac", "Im super dead",2015,9);
-        CD Country = new CD ("Johhny Cash", "Fire",1989,12);
-        CD Jazz = new CD ("Miles Davis", "Miles Ahead",1976,10);
-        ArrayList<CD> allCDs = new ArrayList<CD>();
-        allCDs.add(Rock);
-        allCDs.add(Rap);
-        allCDs.add(Country);
-        allCDs.add(Jazz);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Welcome to our CD store. What would you like to do? Enter one of the following options: Browse selection, Add CD, or Exit");
-        boolean programRunning = true;
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<CD> allCDs = CD.getAll();
+            model.put("allCDs", allCDs);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
-        while (programRunning) {
+        post("/success/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String artist = request.queryParams("artistName");
+            String album = request.queryParams("albumName");
+            int year = Integer.parseInt(request.queryParams("releaseYear"));
+            int price = Integer.parseInt(request.queryParams("price"));
+            CD newCD = new CD(artist,album,year,price);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
-            try {
-                String navigationChoice = bufferedReader.readLine();
-                if (navigationChoice.equals("Browse selection")) {
-                    System.out.println("Do you want to see 'all CDs', filter by 'release year', or filter by 'price'");
-                    String browseChoice = bufferedReader.readLine();
-                    if (browseChoice.equals("all CDs")) {
-                        for (CD individualCD : allCDs) {
-                            System.out.println("----------------------");
-                            System.out.println(individualCD.artistName);
-                            System.out.println(individualCD.albumName);
-                            System.out.println(individualCD.releaseYear);
-                            System.out.println(individualCD.price);
-                        }
-                    } else if (browseChoice.equals("release year")) {
-                        System.out.println("See all albums after this year. Enter year");
-                        int userYearBrowse = Integer.parseInt(bufferedReader.readLine());
-                        for (CD individualCD : allCDs) {
-                            if (individualCD.releaseYear >= userYearBrowse) {
-                                System.out.println("----------------------");
-                                System.out.println(individualCD.artistName);
-                                System.out.println(individualCD.albumName);
-                                System.out.println(individualCD.releaseYear);
-                                System.out.println(individualCD.price);
-                            }
-                        }
-                    } else if (browseChoice.equals("price")) {
-                        System.out.println("Enter your maximum price");
-                        int userMaxPrice = Integer.parseInt(bufferedReader.readLine());
-                        for (CD individualCD : allCDs) {
-                            if (individualCD.price <= userMaxPrice) {
-                                System.out.println("----------------------");
-                                System.out.println(individualCD.artistName);
-                                System.out.println(individualCD.albumName);
-                                System.out.println(individualCD.releaseYear);
-                                System.out.println(individualCD.price);
-                            }
-                        }
-                    }
-                } else if (navigationChoice.equals("Add CD")) {
-                    System.out.println("Alright, let's add a CD! What is the artist's name?");
-                    String userAristName = bufferedReader.readLine();
-                    System.out.println("What is the albumn name?");
-                    String userAlbumnName = bufferedReader.readLine();
-                    System.out.println("What is the release year?");
-                    int userReleaseYear = Integer.parseInt(bufferedReader.readLine());
-                    System.out.println("What is the price?");
-                    int userPrice = Integer.parseInt(bufferedReader.readLine());
 
-                    CD userCD = new CD(userAristName, userAlbumnName, userReleaseYear, userPrice);
 
-                    allCDs.add(userCD);
-
-                    System.out.println("Alright, here's your new vehicle:");
-                    System.out.println("----------------------");
-                    System.out.println(userCD.artistName);
-                    System.out.println(userCD.albumName);
-                    System.out.println(userCD.releaseYear);
-                    System.out.println(userCD.price);
-
-                } else if (navigationChoice.equals("Exit")) {
-                    System.out.println("Goodbye!");
-                    programRunning = false;
-
-                } else {
-                    System.out.println("I'm sorry, we don't recognize your input");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
--- src/main/java/models/CD.java
+++ src/main/java/models/CD.java
@@ -1,19 +1,41 @@
 package models;
 
-/**
- * Created by Guest on 7/31/17.
- */
-public class CD {
-    public String artistName;
-    public String albumName;
-    public int releaseYear;
-    public int price;
 
+import java.util.ArrayList;
+
+public class CD {
+    private String artistName;
+    private String albumName;
+    private int releaseYear;
+    private int price;
+
+    public static ArrayList<CD> allCDs = new ArrayList<CD>();
 
     public CD (String artistName, String albumName, int releaseYear, int price) {
-        this.artistName = artistName ;
+        this.artistName = artistName;
         this.albumName = albumName;
         this.releaseYear = releaseYear;
         this.price = price;
+        allCDs.add(this);
+    }
+
+    public static ArrayList<CD> getAll(){
+        return allCDs;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
